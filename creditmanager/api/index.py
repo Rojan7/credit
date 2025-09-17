0187ea45e7aa2f776e4b1f597100b72ddf3e079f
Rojan7 <rojanadhikari02@gmail.com>
2025-09-17 08:39:54 +0545

    one

--- creditmanager/api/index.py
+++ creditmanager/api/index.py
@@ -1,1 +1,10 @@
-from creditmanager.wsgi import application
+import os
+import sys
+
+# Make sure the root project folder is in sys.path
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from creditmanager.wsgi import application as django_app
+
+# Vercel expects a top-level variable named `app` or `handler`
+app = django_app
